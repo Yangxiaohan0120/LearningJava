4dde23edc0a6cc775dd7aae9b71c82d20da458ed
yxh <17853138921@163.com>
2022-02-14 14:24:49 +0800

    update

---
+++ src/main/java/LC/NQueensII.java
@@ -0,0 +1,13 @@
+package LC;
+
+/**
+ * @ author: yxh
+ * @ created: 2022-02-14 : 2:22 PM
+ */
+public class NQueensII {
+
+    public NQueensII() {
+
+    }
+
+}
---
+++ src/main/java/LC/SudokuSolver.java
@@ -0,0 +1,14 @@
+package LC;
+
+/**
+ * @ author: yxh
+ * @ created: 2022-02-14 : 2:22 PM
+ */
+public class SudokuSolver {
+
+    // 数独问题
+    public SudokuSolver() {
+
+    }
+
+}
