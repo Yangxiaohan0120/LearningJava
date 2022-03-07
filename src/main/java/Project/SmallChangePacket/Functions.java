d90ca5d92478c72c8bffa0cdda384de159ea29d9
牛客401498474号 <17853138921@163.com>
2022-03-07 16:34:16 +0800

    SmallChangePacket

---
+++ src/main/java/Project/SmallChangePacket/Functions.java
@@ -0,0 +1,104 @@
+package Project.SmallChangePacket;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * @author Chris Yang
+ * @program: JavaProject
+ * @description:
+ * @create: 2022-03-07 15:01
+ **/
+public class Functions {
+
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    static Date date;
+    static StringBuilder sb = new StringBuilder();
+    static String record;
+
+    public static void detail(boolean in, int money, File detail) {
+        if (in) {
+            record = "收入";
+        } else {
+            record = "支出";
+        }
+        date = new Date();
+        sb.setLength(0);
+        sb.append(sdf.format(date) + record + money);
+        try {
+            if (!detail.exists()) {
+                detail.createNewFile();
+            }
+            FileWriter fw = new FileWriter(detail, true);
+            fw.write(sb.toString() + "\n");
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getIn(int money, int totalMoney, File detail) {
+        if (money < 0) {
+            System.out.println("数据输入有误，收入应该 > 0");
+        } else {
+            detail(true, money, detail);
+            totalMoney = totalMoney + money;
+            System.out.println("收入" + money);
+        }
+        return totalMoney;
+    }
+
+    public static int getOut(int money, int totalMoney, File detail) {
+        if (money < 0) {
+            System.out.println("数据输入有误，花费应该 > 0");
+        } else if (totalMoney - money < 0) {
+            System.out.println("余额不足");
+        } else {
+            detail(false,money,detail);
+            totalMoney = totalMoney - money;
+            System.out.println("消费" + money);
+        }
+        return totalMoney;
+    }
+
+    public static void getClear(File detail) throws IOException {
+        detail.delete();
+        detail.createNewFile();
+    }
+
+    public static void getPrint() {
+        try {
+            String temp;
+            File directory = new File(".");
+            if (!new File(directory.getAbsolutePath() + "/detail.txt").exists()) {
+                System.out.println("No record");
+            } else {
+                BufferedReader br = new BufferedReader(new FileReader(directory.getAbsolutePath() + "/detail.txt"), 65536);
+                if ((temp = br.readLine()) == null) {
+                    System.out.println("No record");
+                }else {
+                    System.out.println(temp);
+                }
+                while ((temp = br.readLine()) != null) {
+                    System.out.println(temp);
+                }
+                br.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void quit(Scanner scanner) {
+        System.out.println("你确定要退出吗？y/n");
+        if (scanner.hasNext() && scanner.next().equals("y")) {
+            System.out.println("退出了零钱通系统！");
+        } else {
+            System.out.println("你确定要退出吗？y/n");
+        }
+        scanner.close();
+    }
+}
---
+++ src/main/java/Project/SmallChangePacket/Main.java
@@ -0,0 +1,65 @@
+package Project.SmallChangePacket;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author Chris Yang
+ * @create 2022-03-07 14:55
+ **/
+public class Main {
+
+    private String MainMenu = "============Menu===========\n" +
+            "           1. 明细          \n" +
+            "           2. 收入          \n" +
+            "           3. 消费          \n" +
+            "           4. 清除          \n" +
+            "           5. 退出          \n";
+
+    public int TotalMoney = 0;
+    Scanner scanner;
+    File detail = new File("detail.txt");
+    boolean loop = true;
+    String buffered;
+
+    public void start() throws IOException {
+        scanner = new Scanner(System.in);
+        if (detail.exists()) {
+            detail.delete();
+            detail.createNewFile();
+        }
+        do {
+            System.out.println(MainMenu);
+            int key = Integer.parseInt(scanner.next());
+            getOptions(key);
+        } while (loop);
+    }
+
+    public void getOptions(int key) throws IOException {
+        if (key == 1) {
+            Functions.getPrint();
+            System.out.println("结余" + TotalMoney);
+        } else if (key == 2) {
+            System.out.println("请输入金额：");
+            int money = Integer.parseInt(scanner.next());
+            TotalMoney = Functions.getIn(money, TotalMoney, detail);
+        } else if (key == 3) {
+            System.out.println("请输入金额：");
+            int money = Integer.parseInt(scanner.next());
+            TotalMoney = Functions.getOut(money, TotalMoney, detail);
+        } else if (key == 4) {
+            Functions.getClear(detail);
+            TotalMoney = 0;
+        } else if (key == 5) {
+            Functions.quit(scanner);
+            loop = false;
+        } else {
+            System.out.println("Sorry, we don't have " + key + " option!");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().start();
+    }
+}
